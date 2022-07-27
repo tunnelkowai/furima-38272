5f28d1786adb68a9e7abfe3f20337bfa5080b189
tunnelkowai <ueda1410013@yahoo.co.jp>
2022-07-27 19:40:16 +0900

    ユーザー登録・ログイン機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         validates :nick_name,       presence: true
+         validates :last_name,       presence: true
+         validates :first_name,      presence: true
+         validates :last_name_kana,  presence: true
+         validates :first_name_kana, presence: true
+         validates :birthday,        presence: true
+
+         has_many :items
+         has_many :orders
+   
+
+end
