6de9644a1ba5be2b979fb6e4f12937c306e35ecc
tunnelkowai <ueda1410013@yahoo.co.jp>
2022-07-28 20:04:45 +0900

    userモデルの実装要件に含まれない記述をコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,7 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers" }
 
 
-  has_many :items
-  has_many :orders
-   
+  # has_many :items
+  # has_many :orders
 
 end
