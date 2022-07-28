27359528dd93dff05ed159c498a50812738cde67
tunnelkowai <ueda1410013@yahoo.co.jp>
2022-07-28 15:38:02 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,18 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :nick_name,       presence: true
-         validates :last_name,       presence: true
-         validates :first_name,      presence: true
-         validates :last_name_kana,  presence: true
-         validates :first_name_kana, presence: true
-         validates :birthday,        presence: true
+  validates :nick_name,       presence: true
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-ゔァ-ヴ一-龥]/, message: "is invalid. Input full-width characters" }
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-ゔァ-ヴ一-龥]/, message: "is invalid. Input full-width characters"}
+  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
+  validates :birthday,        presence: true
 
-         has_many :items
-         has_many :orders
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers" }
+
+
+  has_many :items
+  has_many :orders
    
 
 end
