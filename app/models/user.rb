3c7e3f25544320de47f77135b5aef719a2864e6e
tunnelkowai <ueda1410013@yahoo.co.jp>
2022-07-28 20:01:35 +0900

    userモデルのバリデーションの正規表現変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nick_name,       presence: true
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-ゔァ-ヴ一-龥]/, message: "is invalid. Input full-width characters" }
-  validates :first_name,      presence: true, format: { with: /\A[ぁ-ゔァ-ヴ一-龥]/, message: "is invalid. Input full-width characters"}
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-ゔァ-ヴ一-龥々ー]/, message: "is invalid. Input full-width characters" }
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-ゔァ-ヴ一-龥々ー]/, message: "is invalid. Input full-width characters"}
   validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters" }
   validates :birthday,        presence: true
