c477dbbcf8a6e2a5230775e16e24db9e8dbd4c1d
tunnelkowai <ueda1410013@yahoo.co.jp>
2022-08-02 14:12:14 +0900

    itemモデルのバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   validates :title,           presence: true, length: { maximum: 40 }
   validates :description,     presence: true, length: { maximum: 1000 }
   validates :price,           presence: true, numericality: { message: "is invalid. Input half-width characters" }
-  validates :price,           numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: " is out of setting range" }
+  validates :price,           numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: " is out of setting range" }
   validates :user,            presence: true
   validates :category_id,     numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id,    numericality: { other_than: 1, message: "can't be blank" }
