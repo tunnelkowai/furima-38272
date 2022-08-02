9864d5f8df6a3b70a3e0cac5608be67fdec3bcc7
tunnelkowai <ueda1410013@yahoo.co.jp>
2022-08-02 09:56:20 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,24 @@
 class Item < ApplicationRecord
+  validates :image,           presence: true
+  validates :title,           presence: true, length: { maximum: 40 }
+  validates :description,     presence: true, length: { maximum: 1000 }
+  validates :price,           presence: true, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }, numericality: { in: 300..9999999, message: " is out of setting range" }
+  validates :user,            presence: true
+  validates :category_id,     numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id,    numericality: { other_than: 1, message: "can't be blank" }
+  validates :postage_id,      numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,   numericality: { other_than: 1, message: "can't be blank" }
+  validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image,           presence: true
+
   belongs_to :user
-  
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :postage
   belongs_to :prefecture
   belongs_to :days_to_ship
+
 end
